01da2112786b40b3d6cd6a35c700d3e6a3fe7dcd
chk777 <93346990+chk777@users.noreply.github.com>
2022-08-29 11:11:01 +0300

    Update main.cpp

--- lesson5/main.cpp
+++ lesson5/main.cpp
@@ -11,18 +11,18 @@ int main()
 	//Task 1
 	cout << "String (random access iterator): ";
 	string test = "aaaa bbbbbbbb aaa aaaa bbbb aa ccccc ccccc cccccc";
-	findUniqueWords(test.begin(), test.end());
+	findWords(test.begin(), test.end());
 	cout << "\nVector (const random access iterator): ";
 	const vector<char> test2 = { 'c', 'c', 'c', ' ', 'a', 'a', 'a', ' ', 'c', 'c', 'c' };
-	findUniqueWords(test2.begin(), test2.end());
+	findWords(test2.begin(), test2.end());
 	cout << "\nList (bidirectional iterator): ";
 	list <char> test3 = { 'c', 'c', 'c', ' ', 'a', 'a', 'a', ' ', 'c', 'c', 'c' };
-	findUniqueWords(test3.begin(), test3.end());
+	findWords(test3.begin(), test3.end());
 	cout << "\nForward list (forward iterator): ";
 	forward_list<char> test4 = { 'c', 'c', 'c', ' ', 'a', 'a', 'a', ' ', 'c', 'c', 'c' };
-	findUniqueWords(test4.begin(), test4.end());
+	findWords(test4.begin(), test4.end());
 	cout << "\n\nTask 2:\n";
 	//Task 2
-	SortLines();
+	sortLines();
 
 }
